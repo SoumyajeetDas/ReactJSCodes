503498aaae026ca238b4cf4117965dd574f8aaf0
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2025-04-19 10:37:41 +0530

    chore: Add comment explaining ref handling in React 19 for InputComponent

--- react19/code/first-code/src/children/InputComponent.tsx
+++ react19/code/first-code/src/children/InputComponent.tsx
@@ -1,5 +1,6 @@
 import { ComponentPropsWithRef } from 'react';
 
+// We don't have to do a forwardRef now in React19. Refs can be passed as props.
 const InputComponent = (props: ComponentPropsWithRef<'input'>) => {
   return <input {...props} />;
 };
